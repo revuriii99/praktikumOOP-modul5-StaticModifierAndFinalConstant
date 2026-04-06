de257d28dca3c9e2016397f4f021ff707285ad04
revuriii99 <reviwilerii@gmail.com>
2026-04-06 21:01:49 +0700

    final

--- src/nomorDua/Main.java
+++ src/nomorDua/Main.java
@@ -5,8 +5,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        Jaket jaket = new Jaket();
 
+        System.out.print("Masukkan Jumlah Jaket A : ");
+        int JumlahA = scanner.nextInt();
+        System.out.print("Masukkan Jumlah Jaket B : ");
+        int JumlahB = scanner.nextInt();
+        System.out.print("Masukkan Jumlah Jaket C : ");
+        int JumlahC = scanner.nextInt();
 
+        System.out.println("Informasi Pembelian : ");
+        jaket.DetailPembelian(JumlahA, JumlahB, JumlahC);
+
+        int Total = jaket.HargaTotal(JumlahA, JumlahB, JumlahC);
+        System.out.print("Total harga: " + Total);
 
         scanner.close();
     }
