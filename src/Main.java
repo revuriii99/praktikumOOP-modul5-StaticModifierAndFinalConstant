55eed97e36d3d8c12835b185ba68889b4f7e46a5
revuriii99 <reviwilerii@gmail.com>
2026-04-06 14:18:50 +0700

    nomor 1 finish

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,33 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Masukkan angka pertama: ");
+        int angka1 = scanner.nextInt();
 
+        System.out.println("Masukkan angka kedua: ");
+        int angka2 = scanner.nextInt();
+        Hitung.penjumlahan(angka1, angka2);
+
+        System.out.println("Masukkan angka pertama: ");
+        int angka3 = scanner.nextInt();
+        System.out.println("Masukkan angka kedua: ");
+        int angka4 = scanner.nextInt();
+        Hitung.pengurangan(angka3, angka4);
+
+        System.out.println("\n");
+        Hitung hitung = new Hitung();
+        System.out.println("Masukkan angka pertama: ");
+        int angka5 = scanner.nextInt();
+        System.out.println("Masukkan angka kedua: ");
+        int angka6 = scanner.nextInt();
+        hitung.perkalian(angka5, angka6);
+        System.out.println("\n");
+
+        System.out.println("Masukkan angka pertama: ");
+        int angka7 = scanner.nextInt();
+        System.out.println("Masukkan angka kedua: ");
+        int angka8 = scanner.nextInt();
+        hitung.pembagian(angka7, angka8);
 
         scanner.close();
     }
